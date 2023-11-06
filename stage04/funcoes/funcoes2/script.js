4698d1db8cabc606184bae602e4e4409a9ae16f6
camilagsoares <camila.soares19@hotmail.com>
2023-11-06 16:05:44 -0300

    Funcoes

--- stage04/funcoes/funcoes2/script.js
+++ stage04/funcoes/funcoes2/script.js
@@ -73,4 +73,13 @@ function Person(name) {
 }
 
 const camila = new Person("Camila")
-console.log(camila.walk())
+// console.log(camila.walk())
+
+let fruit = "banana";
+function getFruit(fruit) {
+  fruit = "orange";
+  return fruit
+}
+
+console.log(fruit);
+console.log(getFruit(fruit));
