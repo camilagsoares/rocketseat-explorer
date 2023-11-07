e810443d5ffe6055af8388919ddef083d2b4eb4d
camilagsoares <camila.soares19@hotmail.com>
2023-11-07 20:15:06 -0300

    aula

--- stage04/condicionaisEControleDeFluxo/script.js
+++ stage04/condicionaisEControleDeFluxo/script.js
@@ -1,3 +1,14 @@
-//Controle e fluxo da aplicação
-console.log('a')
-console.log('b')
+/* Controle e fluxo da aplicação */
+// console.log('a')
+// console.log('b')
+
+/* If...else */
+
+let temperature = 36.5
+if (temperature >= 37.5) {
+    console.log("Febre alta")
+} else if (temperature < 37.5 && temperature >= 37) {
+    console.log("Febre moderada")
+} else {
+    console.log("Saudavel")
+}
