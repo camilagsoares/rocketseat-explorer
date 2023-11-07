d72f9a9c73734a3ae9be286b9cb1209496d879a1
camilagsoares <camila.soares19@hotmail.com>
2023-11-07 20:32:23 -0300

    aula

--- stage04/condicionaisEControleDeFluxo/script.js
+++ stage04/condicionaisEControleDeFluxo/script.js
@@ -2,16 +2,49 @@
 // console.log('a')
 // console.log('b')
 
+
+
+
+
+
+
 /* If...else */
 
 let temperature = 36.5
 let highTemperature = temperature >= 37.5;
 let mediumTemperature = temperature < 37.5 && temperature >= 37;
 
-if (highTemperature) {
-    console.log("Febre alta")
-} else if (mediumTemperature) {
-    console.log("Febre moderada")
-} else {
-    console.log("Saudavel")
+// if (highTemperature) {
+//     console.log("Febre alta")
+// } else if (mediumTemperature) {
+//     console.log("Febre moderada")
+// } else {
+//     console.log("Saudavel")
+// }
+
+
+
+
+
+
+/* Switch */
+
+function calculate(number1, operator, number2) {
+    let result
+
+    switch (operator) {
+        case '+':
+            result = number1 + number2
+            break //mostra que o bloco acabou
+        case '-':
+            result = number1 - number2
+            break
+        default:
+            console.log('default')
+            break
+    }
+    return result
+
 }
+
+console.log(calculate(3,'+',5))
