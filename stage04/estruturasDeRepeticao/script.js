54bd1bbccb4ff2697e75274705fc6be50947483f
camilagsoares <camila.soares19@hotmail.com>
2023-11-07 21:24:03 -0300

    aula

---
+++ stage04/estruturasDeRepeticao/script.js
@@ -0,0 +1,17 @@
+/* Estruturas de Repetição */
+//for
+// break para pausar a execução do loop
+// continue pula a execução do momento
+
+for(let i = 0; i < 10; i++){
+
+    if (i === 5) {
+        continue;
+    }
+    console.log("oi",i)
+}
+
+
+
+
+//while
