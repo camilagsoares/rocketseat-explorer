0eab29048cd824a43c3457a481dd88a8bcec78ee
camilagsoares <camila.soares19@hotmail.com>
2023-11-10 19:45:22 -0300

    Mudancas

---
+++ stage04/somarDoisNumeros/script.js
@@ -0,0 +1,11 @@
+
+const somaNumeros = (number1, number2) => {
+    number1 = Number(prompt("Digite um numero"))
+    number2 = Number(prompt("Digite um numero"))
+
+    console.log("soma", number1 + number2)
+
+}
+
+somaNumeros()
+
