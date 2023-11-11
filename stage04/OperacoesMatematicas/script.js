25c662be6ce4ed515fa2316bb196d758ae884aaa
camilagsoares <camila.soares19@hotmail.com>
2023-11-11 11:24:56 -0300

    Operacoes matematicas

--- stage04/OperacoesMatematicas/script.js
+++ stage04/OperacoesMatematicas/script.js
@@ -1,1 +1,20 @@
 //Operacoes
+
+
+
+function operacoesMatematicas(number1, operador, number2) {
+    number1 = Number(prompt("Digite um número"));
+    number2 = Number(prompt("Digite um número"));
+    operador = prompt("digite um operador")
+
+    switch (operador) {
+        case "soma":
+            operador = "+"
+            break
+        case "subtracao":
+            operador = "+"
+            break
+    }
+
+}
+console.log(operacoesMatematicas(1, "", 2))
