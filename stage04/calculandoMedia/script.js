3284284fbe9ec9de0fd9d6155e404b4e66c989e0
camilagsoares <camila.soares19@hotmail.com>
2023-11-11 13:07:03 -0300

    Calculando media

--- stage04/calculandoMedia/script.js
+++ stage04/calculandoMedia/script.js
@@ -1,19 +1,22 @@
 
-// let nomeAluno = prompt("Oi,qual o seu nome?");
-// let nota1 = Number(prompt("Digite suas notas"));
-// let nota2 = Number(prompt("Digite suas notas"));
-// let nota3 = Number(prompt("Digite suas notas"));
+let nomeAluno = prompt("Oi,qual o seu nome?");
+let nota1 = Number(prompt("Digite suas notas"));
+let nota2 = Number(prompt("Digite suas notas"));
+let nota3 = Number(prompt("Digite suas notas"));
 
 function avaliaNotas() {
 
     let mediaNotas = (nota1 + nota2 + nota3) / 3;
+    mediaNotas = mediaNotas.toFixed(2)
+
+    //Depois do ponto no número,com o toFixed() eu coloco o número que quero mostrar
 
     if (mediaNotas >= 7) {
-        alert("Parabéns! Sua nota foi" + mediaNotas)
+        alert("Parabéns! Sua nota foi " + mediaNotas)
     } else if (mediaNotas < 7) {
-        alert("Poxa,sua nota não foi boa. A nota foi:" + mediaNotas)
+        alert("Poxa,sua nota não foi boa. A nota foi: " + mediaNotas)
     }
 
 }
 
-// avaliaNotas();
+avaliaNotas();
