f05759bee881573f5bd4db00329ab40a0e344165
camilagsoares <camila.soares19@hotmail.com>
2023-11-19 21:39:56 -0300

    Jogo da adivinhacao

--- stage05/jogoAdivinhacao/script.js
+++ stage05/jogoAdivinhacao/script.js
@@ -1,18 +1,31 @@
+//Variáveis
+
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-
-const randomNumber = Math.round(Math.random() * 10);
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+let randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
 
+// Eventos
+//o evento é o click e  o que vai reagir a esse evento que está escutando é a função handleTryClick
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown',function(e){
+    if(e.key == 'Enter' && screen1.classList.contains('.hide')){
+        handleResetClick()
+    }
+})
+
+//Função callback
 function handleTryClick(event) {
     event.preventDefault()
     const inputNumber = document.querySelector("#inputNumber")
 
     if (Number(inputNumber.value) == randomNumber) {
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+        toggleScreen()
 
-        screen2("h2").innerText = `Acertou em ${xAttempts} tentativas`
+        screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
     }
 
     inputNumber.value = ""
@@ -20,16 +33,13 @@ function handleTryClick(event) {
 
 }
 
-// Eventos
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-//o evento é o click e  o que vai reagir a esse evento que está escutando é a função handleTryClick
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function () {
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
-
+function handleResetClick() {
+    toggleScreen();
     xAttempts = 1;
+    randomNumber = Math.round(Math.random() * 10);
+}
 
-})
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
