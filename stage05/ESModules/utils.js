a29dbf1d8b34d71dfff7cd2ab4e34b0a3c1efcc1
camilagsoares <camila.soares19@hotmail.com>
2023-11-25 11:02:20 -0300

    Stage 05

---
+++ stage05/ESModules/utils.js
@@ -0,0 +1,10 @@
+const message = "mensagem"
+const text = "Segundo texto"
+// alert(message)
+
+export { message, text }
+
+export const today = Date.now()
+
+const double = number => number * 2
+export { double }
