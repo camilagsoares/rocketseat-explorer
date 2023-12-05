524371a1fe1665f1806818bbb17254e04b4f2015
camilagsoares <camila.soares19@hotmail.com>
2023-12-04 21:31:39 -0300

    focus timer

--- stage05/focusTimer/FocusTimer/timer.js
+++ stage05/focusTimer/FocusTimer/timer.js
@@ -1,9 +1,10 @@
 import state from "./state.js"
 import * as el from "./elements.js"
 import { reset } from "./actions.js"
+import { kitchenTimer } from "./sounds.js"
 
 export function countdown() {
-    if (!state.isRunning) {
+    if(!state.isRunning) {
         return
     }
 
@@ -12,18 +13,25 @@ export function countdown() {
 
     seconds--
 
-    if (seconds < 0) {
+    if(seconds < 0) {
         seconds = 59
         minutes--
     }
 
     if (minutes < 0) {
         reset()
+        kitchenTimer.play()
         return
     }
+
     updateDisplay(minutes, seconds)
+
+
     setTimeout(() => countdown(), 1000)
+
 }
+
+
 
 export function updateDisplay(minutes, seconds) {
     minutes = minutes ?? state.minutes
