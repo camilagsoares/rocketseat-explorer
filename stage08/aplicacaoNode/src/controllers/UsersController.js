f141bbe5962c5aa3ed4c689a6c6752736104e586
camilagsoares <camila.soares19@hotmail.com>
2023-12-27 18:07:06 -0300

    .

--- stage08/aplicacaoNode/src/controllers/UsersController.js
+++ stage08/aplicacaoNode/src/controllers/UsersController.js
@@ -23,6 +23,24 @@ class UsersController {
 
         return response.status(201).json();
     }
+
+    async update(request, response) {
+        const { name, email } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?) ", [id]);
+
+        if (!user) {
+            throw new AppError("Usuário não encontrado");
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+    
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== id) {
+            throw new AppError("Este e-mail já está em uso.");
+        }
+    }
 }
 
 module.exports = UsersController
