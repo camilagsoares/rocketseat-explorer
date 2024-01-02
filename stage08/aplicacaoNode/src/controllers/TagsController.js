94cc3918391e440a2c6ddbf9729da3a657552dbd
camilagsoares <camila.soares19@hotmail.com>
2024-01-02 11:02:49 -0300

    .

--- stage08/aplicacaoNode/src/controllers/TagsController.js
+++ stage08/aplicacaoNode/src/controllers/TagsController.js
@@ -1,7 +1,15 @@
 const knex = require("../database/knex");
 
 class TagsController {
+    async index(request, response) {
+        const user_id = request.params;
 
+        //isso basicamente diz para o knex ir na tabela tags e filtra onde seja igual o user_id
+        const tags = await knex("tags")
+            .where({ user_id })
+
+        return response.json(tags);
+    }
 }
 
 module.exports = TagsController;
--- stage08/aplicacaoNode/src/routes/index.js
+++ stage08/aplicacaoNode/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express')
 
 const usersRoutes = require('./user.routes')
 const notesRouter = require('./notes.routes')
+const tagsRouter = require('./tags.routes')
 
 const routes = Router();
 
 
 routes.use("/users", usersRoutes);
 routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
 
 module.exports = routes
