11b57e3f87546c18669c81087af031681a7148e3
camilagsoares <camila.soares19@hotmail.com>
2024-01-02 11:05:24 -0300

    .

--- stage08/aplicacaoNode/src/controllers/TagsController.js
+++ stage08/aplicacaoNode/src/controllers/TagsController.js
@@ -2,7 +2,7 @@ const knex = require("../database/knex");
 
 class TagsController {
     async index(request, response) {
-        const user_id = request.params;
+        const { user_id } = request.params;
 
         //isso basicamente diz para o knex ir na tabela tags e filtra onde seja igual o user_id
         const tags = await knex("tags")
