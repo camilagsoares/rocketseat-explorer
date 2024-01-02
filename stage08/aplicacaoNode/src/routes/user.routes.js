3808145bc1b3bfe1d3afa24e04ce9a332b2db3a7
camilagsoares <camila.soares19@hotmail.com>
2024-01-02 08:20:11 -0300

    .

--- stage08/aplicacaoNode/src/routes/user.routes.js
+++ stage08/aplicacaoNode/src/routes/user.routes.js
@@ -3,7 +3,7 @@ const { Router } = require('express')
 const UsersController = require('../controllers/UsersController')
 
 
-const usersRoutes = Router();
+const usersRoutes = Router()
 
 
 const usersController = new UsersController()
