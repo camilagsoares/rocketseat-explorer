28302df13731bdcb8d80909d9d3f030c38ce3526
camilagsoares <camila.soares19@hotmail.com>
2024-01-05 20:13:36 -0300

    .

---
+++ stage09/rocketnotes/src/components/Button/index.jsx
@@ -0,0 +1,17 @@
+import { Container } from "./styles";
+
+
+export const Button = ({ title, loading = false, ...rest }) => {
+
+
+
+    return (
+        <Container
+            type="button"
+            disabled={loading}
+            {...rest}
+        >
+            { loading ? 'Carregando' : title }
+        </Container>
+    )
+}
--- stage09/rocketnotes/src/pages/Details/index.jsx
+++ stage09/rocketnotes/src/pages/Details/index.jsx
@@ -1,13 +1,17 @@
 import './styles.js';
+import { Button } from '../../components/Button/index.jsx';
 
 function Details() {
 
   return (
     <div>
       Hello
+      <Button title="Entrar" />
+      <Button title="Cadastrar" />
+      <Button title="Sair" />
     </div>
   )
-  
+
 }
 
 export default Details;
