02da783e21a22ad8f7bbfb6fdc1d14e55709644b
camilagsoares <camila.soares19@hotmail.com>
2024-01-09 16:11:41 -0300

    .

--- stage09/rocketnotes/src/components/Header/styles.js
+++ stage09/rocketnotes/src/components/Header/styles.js
@@ -42,3 +42,14 @@ align-items: center;
 }
 
 `
+
+export const Logout = styled.button`
+border: none;
+background: none;
+
+
+    > svg{
+        color: ${({ theme }) => theme.COLORS.GRAY_100};
+        font-size: 36px;
+}
+`
