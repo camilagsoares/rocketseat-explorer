2e766895052b0eff7e17fb478ade83b699d932db
camilagsoares <camila.soares19@hotmail.com>
2024-01-13 11:05:25 -0300

    .

--- stage09/rocketnotes/src/main.jsx
+++ stage09/rocketnotes/src/main.jsx
@@ -4,12 +4,13 @@ import { Home } from './pages/Home'
 import { ThemeProvider } from 'styled-components'
 import theme from "./styles/theme"
 import GlobalStyles from "./styles/global"
+import SignIn  from "./pages/SignIn"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
