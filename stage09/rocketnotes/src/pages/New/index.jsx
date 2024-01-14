46f262102ce576ee95660f7f91f2c3778e012e3c
camilagsoares <camila.soares19@hotmail.com>
2024-01-14 17:05:02 -0300

    .

--- stage09/rocketnotes/src/pages/New/index.jsx
+++ stage09/rocketnotes/src/pages/New/index.jsx
@@ -1,6 +1,9 @@
 import { Container, Form } from './styles';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
 
 function New() {
 
@@ -17,7 +20,12 @@ function New() {
 
                     <Input placeholder="Título" />
 
-                    
+                    <Textarea  placeholder="Observações" />
+
+                    <Section title="Links úteis">
+                        <NoteItem />
+                    </Section>
+
                 </Form>
             </main>
         </Container>
