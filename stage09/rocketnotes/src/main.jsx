82de2069716190ae42224171d5c19425613d7f50
camilagsoares <camila.soares19@hotmail.com>
2024-01-14 17:37:01 -0300

    .

--- stage09/rocketnotes/src/main.jsx
+++ stage09/rocketnotes/src/main.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './pages/Home'
 import { ThemeProvider } from 'styled-components'
 import theme from "./styles/theme"
 import GlobalStyles from "./styles/global"
-import SignUp  from "./pages/SignUp"
-import Profile from './pages/Profile'
-import New from './pages/New'
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
