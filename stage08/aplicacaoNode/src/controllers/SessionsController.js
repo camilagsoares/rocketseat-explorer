33e982135ea610ad488530922372d46500378fa7
camilagsoares <camila.soares19@hotmail.com>
2024-01-31 14:24:00 -0300

    .

--- stage08/aplicacaoNode/src/controllers/SessionsController.js
+++ stage08/aplicacaoNode/src/controllers/SessionsController.js
@@ -1,9 +1,19 @@
+const knex = require('../database/knex');
+const AppError = require('../utils/AppError');
+
 class SessionsController {
     async create(request, response) {
 
         const { email, password } = request.body;
 
-        return response.json({ email, password })
+        // knex faz uma busca na tabela "users" onde já existe o e-mail
+        const user = await knex("users").where({ email }).first();
+
+        if (!user) {
+            throw new AppError("Email e/ou senha incorreta", 401);
+        }
+
+        return response.json(user);
     }
 }
 
