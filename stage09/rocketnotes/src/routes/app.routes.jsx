78a90c97130a5719a99bed9e33e7618946ceabdc
camilagsoares <camila.soares19@hotmail.com>
2024-02-28 16:57:17 -0300

    Rotas mudadas

--- stage09/rocketnotes/src/routes/app.routes.jsx
+++ stage09/rocketnotes/src/routes/app.routes.jsx
@@ -1,9 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import New  from '../pages/New';
+import New from '../pages/New';
 import { Home } from '../pages/Home';
-import Profile  from '../pages/Profile';
-import  Details  from '../pages/Details';
+import Profile from '../pages/Profile';
+import Details from '../pages/Details';
 
 
 export function AppRoutes() {
@@ -13,6 +13,7 @@ export function AppRoutes() {
             <Route path="/new" element={<New />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/details/:id" element={<Details />} />
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
 }
