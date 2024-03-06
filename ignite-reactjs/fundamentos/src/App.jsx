f386f91a64a4d80448c72adedb1326eed966a06c
camilagsoares <camila.soares19@hotmail.com>
2024-03-06 16:07:13 -0300

    style module

--- ignite-reactjs/fundamentos/src/App.jsx
+++ ignite-reactjs/fundamentos/src/App.jsx
@@ -1,15 +1,22 @@
 import Post from "./Post"
 import { Header } from './components/Header'
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
 
   return (
     <>
-    <Header />
-      <Post author="Diego" content="lorem" />
-      <Post author="Camila" content="lorem ipsum" />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Diego" content="lorem" />
+          <Post author="Camila" content="lorem ipsum" />
 
+        </main>
+      </div>
     </>
   )
 }
