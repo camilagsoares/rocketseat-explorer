dc989aeb64617bb449ecdccae287d71b6b1ea34f
camilagsoares <camila.soares19@hotmail.com>
2024-03-06 16:29:23 -0300

    finish sidebar

--- ignite-reactjs/fundamentos/src/components/Sidebar.jsx
+++ ignite-reactjs/fundamentos/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
-import styles from './Sidebar.module.css'
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
 
@@ -9,12 +10,19 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar}
+                    src="https://github.com/camilagsoares.png"
+                />
                 <strong>Camila Soares</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
