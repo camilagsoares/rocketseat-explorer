daa6de1bb8040496171f23fc3fa3ae47e726a3ab
camilagsoares <camila.soares19@hotmail.com>
2024-03-09 13:25:58 -0300

    change background sidebar

--- ignite-reactjs/fundamentos/src/components/Sidebar.jsx
+++ ignite-reactjs/fundamentos/src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react'
+import background from '../assets/background.png'
 
 export function Sidebar() {
 
     return (
         <aside className={styles.sidebar}>
             <img className={styles.cover}
-                src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx"
+                src={background}
             />
 
             <div className={styles.profile}>
